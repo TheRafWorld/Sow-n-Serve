ab0cd0f6705abdbfe7f9b82eb183d3e0a5e8d59d
TheRafWorld <rafysamir2@gmail.com>
2025-02-09 12:22:14 -0500

    OMG IT WORKED (Fix All)

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -7,27 +7,27 @@ const NAVBAR_HTML = `
               </button>
       
               <div class="collapse navbar-collapse d-lg-flex" id="Navagation Bar">
-                <a class="navbar-brand col-lg-3 me-0" href="/index.html">
+                <a class="navbar-brand col-lg-3 me-0" href="index.html">
                     <img src="/Assets/Logo.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">Sow N' Serve</a>
                 <ul class="navbar-nav col-lg-6 justify-content-lg-center">
                   <li class="nav-item">
-                    <a class="nav-link active" href="/index.html">Home</a>
+                    <a class="nav-link active" href="index.html">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="/menu.html">Menu</a>
+                    <a class="nav-link" href="menu.html">Menu</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="/reserve.html">Reserve</a>
+                    <a class="nav-link" href="reserve.html">Reserve</a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="/about.html" data-bs-toggle="dropdown" aria-expanded="false">About Us</a>
                     <ul class="dropdown-menu dropdown-menu-end">
-                      <li><a class="dropdown-item" href="/story.html">Our Story</a></li>
-                      <li><a class="dropdown-item" href="/purpose.html">Our Purpose</a></li>
+                      <li><a class="dropdown-item" href="story.html">Our Story</a></li>
+                      <li><a class="dropdown-item" href="purpose.html">Our Purpose</a></li>
                     </ul>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="/referencepage.html">Reference Page</a>
+                    <a class="nav-link" href="referencepage.html">Reference Page</a>
                   </li>
                 </ul>
                 <div class="d-lg-flex col-lg-3 justify-content-end">
